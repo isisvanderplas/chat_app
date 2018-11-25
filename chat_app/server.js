defa4478404362fcc48e6ee0672ce4df0d25b320
Isis van der Plas <isisvanderplas@hotmail.com>
2018-11-25 01:07:48 -0800

    add some front end

--- chat_app/server.js
+++ chat_app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static(__dirname))
+app.use(express.static(__dirname))
 var server = app.listen(3000, () => {
   console.log('server is listening on port ', server.address().port);
 });
