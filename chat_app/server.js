96a8ecd01ac93e9ab0bc66c0dbda80db67d787f7
Isis van der Plas <isisvanderplas@hotmail.com>
2018-11-27 22:15:05 -0800

    create get message service

--- chat_app/server.js
+++ chat_app/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname))
+
+var messages= [
+  {name: 'Tim', message: 'Hi'},
+  {name: 'Tom', message: 'Hello'}
+]
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+})
+
 var server = app.listen(3000, () => {
   console.log('server is listening on port ', server.address().port);
 });
