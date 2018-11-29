d614cc0e5c7f2a7e1b887ad0ec7eaad06343cdce
Isis van der Plas <isisvanderplas@hotmail.com>
2018-11-28 16:21:48 -0800

    refactor using promises, and fix deprecation warnings

--- chat_app/server.js
+++ chat_app/server.js
@@ -9,6 +9,8 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+mongoose.Promise = Promise;
+
 const dbUrl = 'mongodb://user:user10@ds137019.mlab.com:37019/learning-node'
 
 const Message = mongoose.model('Message', {
@@ -25,30 +27,30 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
   var message = new Message(req.body);
 
-  message.save((err) =>{
-    if(err)
-      sendStatus(500);
-
-    Message.findOne({message: 'badword'}, (err, censored) => {
-      if(censored) {
-        console.log('censored words found', censored);
-        Message.remove({_id: censored.id}, (err) => {
-          console.log('removed censored message');
-        })
-      }
-    })
-
+  message.save()
+  .then(() => {
+    console.log('saved');
+    return Message.findOne({message: 'badword'});
+  })
+  .then( censored => {
+    if(censored) {
+      console.log('censored words found', censored);
+        return Message.deleteOne({_id: censored.id});
+    }
     io.emit('message', req.body);
     res.sendStatus(200);
   })
-
+  .catch((err) => {
+    res.sendStatus(500);
+    return console.error(err);
+  })
 })
 
 io.on('connection', (socket) => {
   console.log('a user is connected');
 })
 
-mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => {
   console.log('mongodb connection', err);
 })
 
